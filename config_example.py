368fd8a42b2d4e6a5ba95771eb0b02c5e4f29dda
Bertimaz <albert.mazuz@gmail.com>
2022-11-08 10:06:33 -0300

    Update and rename config.py to config_example.py

--- config_example.py
+++ config_example.py
@@ -1,14 +1,14 @@
 import os
 
-SECRET_KEY= 'albert'
+SECRET_KEY= ''
 
 SQLALCHEMY_DATABASE_URI = \
 '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
     SGBD = 'mysql+mysqlconnector',
-    usuario = 'root',
-    senha = '30794009',
-    servidor = 'localhost',
-    database = 'jogoteca'
+    usuario = '',
+    senha = '',
+    servidor = '',
+    database = ''
 )
 
 UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__))+'/uploads'
